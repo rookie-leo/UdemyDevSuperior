3ea5f15b5d3d63da19935a712db9d1d1d239f865
rookie-leo <proleonarpa@gmail.com>
2021-04-22 07:46:17 -0300

    Aula de Herança

---
+++ src/heranca/application/Program.java
@@ -0,0 +1,45 @@
+package heranca.application;
+
+import heranca.entities.Account;
+import heranca.entities.BusinessAccount;
+import heranca.entities.SavingsAccount;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Account acc = new Account(1001, "Alex", 0.0);
+		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+		/**
+		 * UPCASTING 
+		 * Casting da subclasse para superclasse
+		 */
+		Account acc1 = bacc;
+		Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+		Account acc3 = new SavingsAccount(1004, "Ana", 0.0, 0.01);
+		
+		/**
+		 * DOWNCASTING 
+		 * Casting da superclasse para subclasse
+		 */
+		BusinessAccount acc4 = (BusinessAccount) acc2;
+		acc4.loan(200.0);
+		
+//		BusinessAccount acc5 = (BusinessAccount) acc3; // Gera um erro de compilacao
+		
+		// Testa se a variavel acc3 eh uma instancia de BusinessAccount
+		if (acc3 instanceof BusinessAccount) {
+			BusinessAccount acc5 = (BusinessAccount) acc3;
+			acc5.loan(200.0);
+			System.out.println("Loan!");
+		}
+		
+		// Testa se a variavel acc3 eh uma instancia de SavingsAccount
+		if (acc3 instanceof SavingsAccount) {
+			SavingsAccount acc5 = (SavingsAccount)acc3;
+			acc5.updateBalance();
+			System.out.println("Update!");
+		}
+	}
+}
---
+++ src/heranca/entities/Account.java
@@ -0,0 +1,49 @@
+package heranca.entities;
+
+public class Account {
+
+	private Integer number;
+	private String holder;
+	protected Double balance;
+	
+	public Account() {
+	}
+
+	public Account(Integer number, String holder, Double balance) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+	}
+
+//	Getters and Setters
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	/*Metodo para saque*/
+	public void withdraw(double amount) {
+		balance -= amount;
+	}
+	
+	/*Metodo para deposito*/
+	public void deposit(double amount) {
+		balance += amount;
+	}
+	
+}
