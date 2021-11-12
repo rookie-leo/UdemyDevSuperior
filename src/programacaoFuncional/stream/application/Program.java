772cd84edcf009f226997561ca45c43785c17fa8
rookie-leo <proleonarpa@gmail.com>
2021-11-12 11:52:42 -0300

    Trabalhando com Stream

---
+++ src/programacaoFuncional/stream/application/Program.java
@@ -0,0 +1,28 @@
+package programacaoFuncional.stream.application;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		List<Integer> list = Arrays.asList(3, 4, 10, 15, 20, 25);
+		Stream<Integer> st1 = list.stream().map(x -> x * 10);
+		System.out.println(Arrays.toString(st1.toArray()));
+		
+		Stream<String> st2 = Stream.of("Maria", "João", "Ana", "José");
+		System.out.println(Arrays.toString(st2.toArray()));
+		
+		Stream<Integer> st3 = Stream.iterate(0, x -> x + 2);
+		System.out.println(Arrays.toString(st3.limit(10).toArray()));
+		
+		Stream<Long> st4 = Stream.iterate(new Long[] {0L, 1L}, 
+					p -> new Long[] {p[1], p[0] + p[1]})
+				.map(p -> p[0]);
+		System.out.println(Arrays.deepToString(st4.limit(11).toArray()));
+		
+	}
+	
+}
